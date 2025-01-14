13eb4afe88007612af1ebd6ac97ae8d24bbafb37
c-dawg89 <615715@fom-net.de>
2025-01-14 10:43:25 +0100

    ändern des tab namens

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
 
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Ihr Besen Shop - Login</title>
+    <title>Liquor Store</title>
     <!-- Bootstrap CSS -->
     <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
     <style>
